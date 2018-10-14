c955c746bdb63cb5c19bc9e196659edb231a1230
unknown <swati.ghadge03@gmail.com>
2018-10-14 16:22:22 +0530

    Project add for first time

---
+++ src/main/java/com/technocredits/orangehrm/pages/LoginPage.java
@@ -0,0 +1,14 @@
+package com.technocredits.orangehrm.pages;
+
+public class LoginPage {
+	private static 
+	private LoginPage()
+	{
+		
+	}
+	
+ public void login(String locator,String value )
+ {
+	 
+ }
+}
